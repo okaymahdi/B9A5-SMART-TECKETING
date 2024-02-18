c000f08edad0905e0be88d09106237e88a82b325
okaymahdi <mahdimunshee@gmail.com>
2024-02-18 10:41:48 +0600

    added navbar, header section & main section

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+        fontFamily: {
+            'Raleway': ['Raleway', 'sans-serif'],
+            'Inter': ['Inter', 'sans-serif'],
+        }
+    },
+    plugins: [],
+}
